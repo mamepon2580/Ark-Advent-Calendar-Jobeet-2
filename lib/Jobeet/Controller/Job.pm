d510468aea56fac955e531d94a0feda217679328
Akifumi Takahashi <akipaka@localhost.localdomain>
2020-09-05 14:02:39 -0400

    コントローラとのつなぎこみ

--- lib/Jobeet/Controller/Job.pm
+++ lib/Jobeet/Controller/Job.pm
@@ -1,8 +1,11 @@
 package Jobeet::Controller::Job;
 use Ark 'Controller';
+with 'Ark::ActionClass::Form';
 
 use Jobeet::Models;
 has '+namespace' => default => 'job';
+
+__PACKAGE__->meta->make_immutable;
 
 sub index :Path {
     my ($self, $c) = @_;
@@ -13,28 +16,63 @@ sub index :Path {
 # /job/{job_token} （詳細）
 sub show :Path :Args(1) {
     my ($self, $c, $job_token) = @_;
+
+    $c->stash->{job} = models('Schema::Job')->find({ token => $job_token })
+        or $c->detach('/default');
 }
 
 # /job/create （新規作成）
-sub create :Local {
+sub create :Local :Form('Jobeet::Form::Job') {
     my ($self, $c) = @_;
+
+    if ($c->req->method eq 'POST' and $self->form->submitted_and_valid) {
+        my $job = models('Schema::Job')->create_from_form($self->form);
+        $c->redirect( $c->uri_for('/job', $job->token) );
+    }
 }
 
 sub job :Chained('/') :PathPart :CaptureArgs(1) {
     my ($self, $c, $job_token) = @_;
-    $c->stash->{job_token} = $job_token;
+
+    $c->stash->{job} = models('Schema::Job')->find({ token => $job_token })
+        or $c->detach('/default');
 }
 
 # /job/{job_token}/edit （編集）
-sub edit :Chained('job') :PathPart :Args(0) {
+sub edit :Chained('job') :PathPart :Form('Jobeet::Form::Job') {
     my ($self, $c) = @_;
+
+    my $job = $c->stash->{job};
+
+    if ($c->req->method eq 'POST') {
+        if ($self->form->submitted_and_valid) {
+            $job->update_from_form($self->form);
+            $c->redirect( $c->uri_for('/job', $job->token) );
+        }
+    }
+    else {
+        $self->form->fill({
+            $job->get_columns,
+            category => $job->category->slug,
+        });
+    }
 }
 
 # /job/{job_token}/delete （削除）
-sub delete :Chained('job') :PathPart :Args(0) {
+sub delete :Chained('job') :PathPart {
     my ($self, $c) = @_;
+
+    $c->stash->{job}->delete;
+    $c->redirect( $c->uri_for('/job') );
 }
 
-__PACKAGE__->meta->make_immutable;
+sub publish :Chained('job') :PathPart {
+    my ($self, $c) = @_;
+
+    my $job = $c->stash->{job};
+
+    $job->publish;
+    $c->redirect( $c->uri_for('/job', $job->token) );
+}
 
 1;
